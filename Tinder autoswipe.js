cae7c4a0a17c741c10f6cb8d26705343b7e01ec3
usamakhankhattakk <60480160+usamakhankhattakk@users.noreply.github.com>
2023-04-15 20:17:57 +0500

    Update Tinder autoswipe.js

--- Tinder autoswipe.js
+++ Tinder autoswipe.js
@@ -1,54 +1,52 @@
-
-  
 var confirmBox = confirm('Start swiping right?');
 var count = 1;
 var k = 0;
 //input how many likes you want
-var likes= 5;
+var likes = 5;
 
+const likebutton = document.querySelector('[aria-label="Like"]');
 
+//intialiation function to run swiping
+check();
 
- function liked(){
-    if (confirmBox){
-      document.querySelector('[aria-label="Like"]').click();
-      console.log('Liked ' + count);						  
-      count++;
-      k++;												};
-    
-};
 
 //checking to like users 
-function check(){
-if (k<likes)
-{
+function check() {
 
-like();
-//checking after every 3 sec
-setTimeout(check,5000);
-
-}
+  if (!confirmBox) {
+    console.log('Swiping cancelled.');
+  } else if (!likebutton) {
+    console.error('Like button not found.');
+  } else {
+    if (k < likes) {
+      like();
+      //checking after every 5 sec
+      setTimeout(check, 5000);
+    }
+  };
 };
-
-
-
-
 
 
 //like 3 users with every 1 sec difference between them
- function like(){
-   let i;
-    for ( i = 0; i <3; i++){
+function like() {
+  let i;
+  for (i = 0; i < 3; i++) {
     task(i);
-   };
-  function task(i){
-    setTimeout(liked,1000*i);
-    };
-    
+  };
 };
 
-//intialiation function to run swiping
- check();
+function task(i) {
+  setTimeout(liked, 1000 * i);
+};
+
+
+function liked() {
+
+  likebutton.click();
+  console.log('Liked ' + count);
+  count++;
+  k++;
+};
+
 
 // 1000 millisec=1sec
-
-
