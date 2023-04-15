6487d28a8906a512142e9a9896a7c119fc297d14
usamakhankhattakk <60480160+usamakhankhattakk@users.noreply.github.com>
2023-04-15 20:23:16 +0500

    Update Tinder autoswipe.js

--- Tinder autoswipe.js
+++ Tinder autoswipe.js
@@ -4,44 +4,38 @@ var k = 0;
 //input how many likes you want
 var likes = 5;
 
-const likebutton = document.querySelector('[aria-label="Like"]');
-
 //intialiation function to run swiping
 check();
 
-
 //checking to like users 
 function check() {
-
+  const likebutton = document.querySelector('[aria-label="Like"]');
   if (!confirmBox) {
     console.log('Swiping cancelled.');
   } else if (!likebutton) {
     console.error('Like button not found.');
   } else {
     if (k < likes) {
-      like();
+      like(likebutton);
       //checking after every 5 sec
       setTimeout(check, 5000);
     }
   };
 };
 
-
 //like 3 users with every 1 sec difference between them
-function like() {
+function like(likebutton) {
   let i;
   for (i = 0; i < 3; i++) {
-    task(i);
+    task(i, likebutton);
   };
 };
 
-function task(i) {
-  setTimeout(liked, 1000 * i);
+function task(i, likebutton) {
+  setTimeout(() => liked(likebutton), 1000 * i);
 };
 
-
-function liked() {
-
+function liked(likebutton) {
   likebutton.click();
   console.log('Liked ' + count);
   count++;
@@ -50,3 +44,4 @@ function liked() {
 
 
 // 1000 millisec=1sec
+
